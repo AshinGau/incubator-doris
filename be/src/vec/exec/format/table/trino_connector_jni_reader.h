6b9bd7b26af565e426b449683c6a814bbf3ea05b
gaoxin <AshinGau@outlook.com>
2024-05-20 09:09:47 +0800

    JniReader

--- be/src/vec/exec/format/table/trino_connector_jni_reader.h
+++ be/src/vec/exec/format/table/trino_connector_jni_reader.h
@@ -27,8 +27,7 @@
 
 #include "common/status.h"
 #include "exec/olap_common.h"
-#include "vec/exec/format/generic_reader.h"
-#include "vec/exec/jni_connector.h"
+#include "vec/exec/format/jni_reader.h"
 
 namespace doris {
 class RuntimeProfile;
@@ -42,7 +41,7 @@ struct TypeDescriptor;
 
 namespace doris::vectorized {
 
-class TrinoConnectorJniReader : public GenericReader {
+class TrinoConnectorJniReader : public JniReader {
     ENABLE_FACTORY_CREATOR(TrinoConnectorJniReader);
 
 public:
@@ -61,18 +60,7 @@ public:
     Status init_reader(
             std::unordered_map<std::string, ColumnValueRangeType>* colname_to_value_range);
 
-    Status close() override {
-        if (_jni_connector) {
-            return _jni_connector->close();
-        }
-        return Status::OK();
-    }
-
 private:
     Status _set_spi_plugins_dir();
-    const std::vector<SlotDescriptor*>& _file_slot_descs;
-    RuntimeState* _state;
-    RuntimeProfile* _profile;
-    std::unique_ptr<JniConnector> _jni_connector;
 };
 } // namespace doris::vectorized
